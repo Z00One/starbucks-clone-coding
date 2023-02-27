075d12caead9a9dc418299c909a0ee5730c57574
Z00One <9000248@g.yju.ac.kr>
2023-02-27 20:28:57 +0900

    Making Element Slide

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -39,9 +39,48 @@ window.addEventListener('scroll', _.throttle(function () {
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 
 // Set the delay value to 0.7 seconds difference for each element
-fadeEls.forEach(function(fadeEl, index) {
+fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
     opacity: 1,
     delay: (index + 1) * .7,
   });
+});
+
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+new Swiper('.promotion .swiper', {
+  // direction: 'horizontal', -> default value
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  // autoplay: {
+  //   delay: 5000 // 5000ms
+  // }
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
 })
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide');
+  }
+  else {
+    promotionEl.classList.remove('hide');
+  }
+});
